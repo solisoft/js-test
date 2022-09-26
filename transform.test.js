c037002b6c57577136613f265c462f14d2c691a0
BONNAURE Olivier <o.bonnaure@solisoft.net>
2022-09-26 16:16:27 +0200

    hide code

--- transform.test.js
+++ transform.test.js
@@ -1,41 +1,17 @@
 const { translate2d, scale2d, composeTransformation, memoizeTransform } = require('./transform');
 
 test('move to the right by 2px', () => {
-  const moveCoordinatesRight2Px = translate2d(2, 0)
-  expect(moveCoordinatesRight2Px(4, 8).join(",")).toBe("6,8");
+  expect(true).toBe(false);
 });
 
 test('scale * 2', () => {
-  const doubleScale = scale2d(2, 2)
-  expect(doubleScale(6, -3).join(",")).toBe("12,-6");
+  expect(true).toBe(false);
 });
 
 test('composeTransformation', () => {
-  const moveCoordinatesRight2Px = translate2d(2, 0);
-  const doubleCoordinates = scale2d(2, 2);
-  const composedTransformations = composeTransformation(
-    moveCoordinatesRight2Px,
-    doubleCoordinates
-  );  
-  expect(composedTransformations(0, 1).join(",")).toBe("4,2");
+  expect(true).toBe(false);
 });
 
 test('memoizeTransform', () => {
-  const tripleScale = scale2d(3, 3);
-  const memoizedScale = memoizeTransform(tripleScale);
-  var result = memoizedScale(4, 3)
-  expect(result.value.join(",")).toBe("12,9")
-  expect(result.from_cache).toBe(false)
-  
-  result = memoizedScale(4, 3)
-  expect(result.value.join(",")).toBe("12,9")
-  expect(result.from_cache).toBe(true)
-  
-  result = memoizedScale(3, 3)
-  expect(result.value.join(",")).toBe("9,9")
-  expect(result.from_cache).toBe(false)
-  
-  var result = memoizedScale(4, 3)
-  expect(result.value.join(",")).toBe("12,9")
-  expect(result.from_cache).toBe(false)
+  expect(true).toBe(false);
 })
